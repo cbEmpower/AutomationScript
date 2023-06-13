bc66d2a24fc2d959f74fd62ef26b91c69455e49c
614719121 <614719121@BT-018385320857>
2023-06-13 14:21:53 +0100

    Add project files.

---
+++ EmpowerTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("13FB3248-2C16-4B5E-BE69-CCEA4F1451D2")]
+[assembly: AssemblyVersion("1.0.0.0")]
